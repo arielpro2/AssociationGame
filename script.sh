6a916af5570dcec56c8b5f375ae0d4de8b46b32c
Ariel Shokrany <37239599+arielpro2@users.noreply.github.com>
2022-11-20 13:27:25 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ python3 -m virtualenv venv
 export PYTHONUNBUFFERED=1
 export DEBUG=True
 export FLASK_SECRETKEY=dev
-export POSTGRES_HOSTlocalhost
+export POSTGRES_HOST=localhost
 export POSTGRES_PASS=arieljan04
 export POSTGRES_PORT=5432
 export POSTGRES_USER=postgres
