2682a6a5b2bba38b4d86606d5218a182977a4657
ariel <arielshokrany04@gmail.com>
2022-11-22 01:01:27 +0200

    second commit

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask_socketio import SocketIO
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{os.environ["POSTGRES_USER"]}:{os.environ["POSTGRES_PASS"]}@{os.environ["POSTGRES_HOST"]}:{os.environ["POSTGRES_PORT"]}/db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{os.environ["POSTGRES_USER"]}:{os.environ["POSTGRES_PASS"]}@{os.environ["POSTGRES_HOST"]}:{os.environ["POSTGRES_PORT"]}/network'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config['CACHE_TYPE'] = 'SimpleCache'
 app.config['CACHE_DEFAULT_TIMEOUT'] = 0
@@ -58,7 +58,18 @@ def getNetworkFromSql():
     nodes = {str(node.id): {'id':str(node.id),'label':str(node.label)} for node in nodes_query}
     label2id = {str(node.label): str(node.id) for node in nodes_query}
     edges = {f'{min(edge.from_id,edge.to_id)}-{max(edge.from_id,edge.to_id)}': {'id':str(edge.id),'label':str(edge.votes), 'from':str(edge.from_id), 'to':str(edge.to_id)} for edge in edges_query}
-    return {'nodes':nodes,'edges':edges, 'label2id':label2id}
+    nodeConnections = {}
+    for edge in edges_query:
+        from_id,to_id = edge.from_id,edge.to_id
+        if str(from_id) not in nodeConnections:
+            nodeConnections[str(from_id)] = [str(to_id)]
+        else:
+            nodeConnections[str(from_id)].append(str(to_id))
+        if str(to_id) not in nodeConnections:
+            nodeConnections[str(to_id)] = [str(from_id)]
+        else:
+            nodeConnections[str(to_id)].append(str(from_id))
+    return {'nodes':nodes,'edges':edges, 'label2id':label2id, 'nodeConnections':nodeConnections}
 
 def updateSqlFromCache():
     cache_node_list = list(cache.get('nodes').values())
@@ -74,17 +85,17 @@ def updateSqlFromCache():
     print('Network Saved to DB')
 
 
-
 #startup
 @app.before_first_request
-def initializeCache():
+def initialize():
     network = getNetworkFromSql()
     cache.set('nodeCount', len(network['nodes']))
     cache.set('edgeCount', len(network['edges']))
+    cache.set('nodeConnections', network['nodeConnections'])
     cache.set('nodes', network['nodes'])
     cache.set('edges', network['edges'])
     cache.set('label2id', network['label2id'])
-    scheduler.add_job(func=updateSqlFromCache, trigger="interval", seconds=60)
+    scheduler.add_job(func=updateSqlFromCache, trigger="interval", seconds=10)
     scheduler.start()
 
 #Pages
@@ -95,15 +106,34 @@ def Main():
 @app.route('/graph', methods=['GET'])
 def Graph():
     nodes = cache.get('nodes')
-    edges = cache.get('edges')
-    return render_template('graph.html', nodes=list(nodes.values()), edges=list(edges.values()))
+    connected = getConnected('0')
+    node_result = [nodes['0']]
+    node_result[0]['color'] = 'orange'
+    node_result += connected['nodes']
+    return render_template('graph.html', nodes=node_result, edges=connected['edges'])
+
 
 #API
 @app.route('/api/getNetwork', methods=['GET'])
 def getNetwork():
     return json.dumps(getNetworkFromSql())
 
-#@limiter.limit("10/minute")
+@app.route('/api/getConnected', methods=['GET'])
+def getConnected(from_id=None):
+    if not from_id:
+        from_id = request.args.get('from_id')
+    nodeConnections = cache.get('nodeConnections')
+    result = {}
+    if from_id in nodeConnections:
+        nodeConnections = nodeConnections[from_id]
+        from_id = int(from_id)
+        edges = cache.get('edges')
+        nodes = cache.get('nodes')
+        result['edges'] = [edges[f'{min(from_id,int(node_id))}-{max(from_id,int(node_id))}'] for node_id in nodeConnections]
+        result['nodes'] = [nodes[node_id] for node_id in nodeConnections]
+        return result
+    return {'edges':[], 'nodes':[]}
+
 @app.route('/api/addNode', methods=['POST'])
 def addNode():
     label = request.form["label"]
@@ -112,6 +142,7 @@ def addNode():
     temp_nodes = cache.get('nodes')
     temp_label2id = cache.get('label2id')
     temp_edges = cache.get('edges')
+    temp_nodeConnections = cache.get('nodeConnections')
 
     if from_id not in temp_nodes:
         return 'From id doesnt exist'
@@ -124,20 +155,24 @@ def addNode():
         return 'Incorrect label format'
 
     user_cache = cache.get(request.remote_addr)
-    #if user_cache:
-    #    if {'from_label': from_label, 'label': label} in user_cache or {'from_label': label, 'label':from_label } in user_cache:
-    #        return 'Already Requested'
+    if user_cache:
+        if {'from_label': from_label, 'label': label} in user_cache or {'from_label': label, 'label':from_label } in user_cache:
+            return 'Already Requested'
 
+    temp_node = None
+    temp_edge = None
     #Node handling
     if label not in temp_label2id:
+        #New node
         to_id = cache.get('nodeCount')
-        temp_nodes[str(to_id)] = {'id': str(to_id), 'label':label}
-        socketio.emit('newNode', temp_nodes[str(to_id)])
+        temp_node = {'id': str(to_id), 'label':label}
+        temp_nodes[str(to_id)] = temp_node
         cache.set('nodeCount', to_id+1)
         cache.set('nodes', temp_nodes)
         temp_label2id[label] = str(to_id)
         cache.set('label2id', temp_label2id)
     else:
+        #Existing node
         to_id = cache.get('label2id')[label]
 
     #Edge handling
@@ -146,13 +181,23 @@ def addNode():
         temp_edge = temp_edges[f'{min(int(from_id),int(to_id))}-{max(int(from_id),int(to_id))}']
         temp_edge['label'] = str(int(temp_edge['label'])+1)
         temp_edges[f'{min(from_id,to_id)}-{max(from_id,to_id)}'] = temp_edge
-        socketio.emit('updateEdge', temp_edge)
         cache.set('edges', temp_edges)
     else:
         #New edge
         edge_id = cache.get('edgeCount')
-        temp_edges[f'{min(int(from_id),int(to_id))}-{max(int(from_id),int(to_id))}'] = {'id': edge_id, 'from': from_id, 'to':to_id, 'label':'1'}
-        socketio.emit('newEdge', temp_edges[f'{min(int(from_id),int(to_id))}-{max(int(from_id),int(to_id))}'])
+        temp_edge = {'id': edge_id, 'from': from_id, 'to':str(to_id), 'label':'1'}
+        temp_edges[f'{min(int(from_id),int(to_id))}-{max(int(from_id),int(to_id))}'] = temp_edge
+
+        if str(from_id) not in temp_nodeConnections:
+            temp_nodeConnections[str(from_id)] = [str(to_id)]
+        else:
+            temp_nodeConnections[str(from_id)].append(str(to_id))
+        if str(to_id) not in temp_nodeConnections:
+            temp_nodeConnections[str(to_id)] = [str(from_id)]
+        else:
+            temp_nodeConnections[str(to_id)].append(str(from_id))
+
+        cache.set('nodeConnections', temp_nodeConnections)
         cache.set('edgeCount', edge_id + 1)
         cache.set('edges', temp_edges)
 
@@ -163,6 +208,13 @@ def addNode():
     else:
         cache.set(request.remote_addr, [{'from_label':from_label, 'label':label}])
 
+    socket_messege = {}
+    if temp_node:
+        socket_messege['node'] = temp_node
+    if temp_edge:
+        socket_messege['edge'] = temp_edge
+
+    socketio.emit('newConnection',socket_messege)
     return 'Success'
 
 
