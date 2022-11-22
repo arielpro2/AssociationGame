eba88e1f87a46ff14463cdea8dd70054e0b3d002
ariel <arielshokrany04@gmail.com>
2022-11-22 18:59:14 +0200

    second commit

--- app.py
+++ app.py
@@ -220,7 +220,7 @@ def addNode():
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=os.environ['DEBUG'] == 'True', host='0.0.0.0')
+    socketio.run(app, debug=os.environ['DEBUG'] == 'True', host='0.0.0.0', port=80)
 
 
 
